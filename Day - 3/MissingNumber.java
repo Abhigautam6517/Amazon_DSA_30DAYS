90e9b41be5bbdba37fe2ab634dd1101f782cbb34
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-09-27 12:12:12 +0530

    Add files via upload

---
+++ Day - 3/MissingNumber.java
@@ -0,0 +1,16 @@
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+          int sum = 0;
+          int res = 0;
+        int total  =( n*(n+1)/2);
+          for(int i =0;i<n;i++){
+              sum += nums[i];
+              res= total-sum;
+  
+          }
+          return res;
+  }
+    
+}
